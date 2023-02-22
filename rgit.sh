3a1c3bd9d30efa28b8ccd687141caa717aff8151
tibofordeyn <tibofordeyn5@gmail.com>
2023-02-22 15:48:00 +0100

    Notities geupdate

--- rgit.sh
+++ rgit.sh
@@ -3,7 +3,9 @@
 git add -A
 git commit -m "Notities geupdate"
 git push -u origin
-#expect "username for "
+# expect "username for "
+# stel nu je moet nog een prompt beantwoorden uiteindelijk, dan zul je expect
+# moeten gebruiken
 
 
 
